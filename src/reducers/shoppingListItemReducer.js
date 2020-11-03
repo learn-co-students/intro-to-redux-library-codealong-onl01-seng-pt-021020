5af6cb7f0bf15eba36669edc995377fe4c1cf451
github username <github email address>
2020-11-03 13:55:53 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,15 +1,11 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+export default function shoppingListItemReducer(state = { items: [] }, action) {
+	console.log(action)
 	switch (action.type) {
 		case 'INCREASE_COUNT':
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
-			}
+			};
 		default:
 			return state;
 	}
